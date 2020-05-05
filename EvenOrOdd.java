82a672914de8cc0bc36316385121beb1088e3041
hbently <59975001+hbently@users.noreply.github.com>
2020-05-05 14:10:06 -0400

    Update EvenOrOdd.java

--- EvenOrOdd.java
+++ EvenOrOdd.java
@@ -1,26 +1,28 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
  * @author hunterbentley
  */
+
+
+// Import the scanner so that user input can be read
 import java.util.Scanner;
     
 public class EvenOrOdd {
 
     public static void main(String[] args) {
-
+        
+        // User input can be read
         Scanner reader = new Scanner(System.in);
 
+        // Print out "Enter a number" to the user
         System.out.print("Enter a number: ");
+        // Assign variable "num" to whatever number the user inputs
         int num = reader.nextInt();
 
+        // If the number inputted by the user divided by two is equal to 0 print that the number is even 
         if(num % 2 == 0)
             System.out.println(num + " is even");
+        // If the number inputted by the user divided by two is not equal to 0 print that the number is odd 
         else
             System.out.println(num + " is odd");
     }
